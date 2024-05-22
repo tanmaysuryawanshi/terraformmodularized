41d3332df4e8109fc110b91f6cd08cc54258faad
Tanmay Suryawanshi <tanmaysuryawanshi123@gmail.com>
2024-05-22 12:25:34 +0530

    init

---
+++ main.tf
@@ -0,0 +1,9 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example" {
+  ami = ""
+  instance_type = ""
+  subnet_id = ""
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+ami_value = "ami-0bb84b8ffd87024d8"
+instance_type_value = "t2.micro"
